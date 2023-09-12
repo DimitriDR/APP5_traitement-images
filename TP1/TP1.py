ea511d8580c61e9134b882cfb8f14e5ab0358959
Andre <85369189+andrbtn@users.noreply.github.com>
2023-09-13 00:26:07 +0200

    fix black fragments with alpha + reposition of fragments TODO fix out of range

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -4,32 +4,51 @@ import numpy as np
 img_width = 1707
 img_height = 775
 
-reconstructed_image = np.zeros((img_height, img_width, 3), dtype=np.uint8)
-
+reconstructed_image = np.zeros((img_height, img_width, 4), dtype=np.uint8)
+reconstructed_image.fill(255)
+print("reconstructed_image ", reconstructed_image.shape)
 with open("./assets/fragments.txt", "r") as frag_direction_file:
     for line in frag_direction_file:
+        # print("-------------------")
         line: str = line.strip()
-        part: list[str] = line.split(" ")
+        parts: list[str] = line.split(" ")
 
-        next_parts: list[str] = part[1:]
+        x: int = int(parts[1])
+        y: int = int(parts[2])
+        angle: float = float(parts[3])
+        # print("x " + str(x) + " y " + str(y) + " angle " + str(angle) + " index " + parts[0])
 
-        x: int = int(next_parts[0])
-        y: int = int(next_parts[1])
-        angle: float = float(next_parts[2])
-
-        frag_location: str = "./assets/frag_eroded/frag_eroded_" + part[0] + ".png"
-
-        fragment = cv2.imread(frag_location)
-
-        rows, cols, _ = fragment.shape
-        M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
-        fragment = cv2.warpAffine(fragment, M, (cols, rows))
-
+        frag_location: str = "./assets/frag_eroded/frag_eroded_" + parts[0] + ".png"
+        fragment = cv2.imread(frag_location, cv2.IMREAD_UNCHANGED)
+        h = fragment.shape[0]
+        w = fragment.shape[1]
+        # height(nb rows), width(nb cols), channels <= .shape
         x_pos = int(x)
         y_pos = int(y)
 
-        if x_pos >= 0 and y_pos >= 0 and x_pos + cols <= img_width and y_pos + rows <= img_height:
-            reconstructed_image[y_pos:y_pos + rows, x_pos:x_pos + cols] = fragment
+        # print("fragment ", fragment.shape)
+        M = cv2.getRotationMatrix2D(center=(w // 2, h // 2), angle=angle, scale=1)
+        fragment = cv2.warpAffine(fragment, M, (w, h))
+
+        # print("x_pos " + str(x_pos) + ", y_pos " + str(y_pos))
+
+        # pour éviter les pbs d'out of range, on prend la différence et pas juste // 2 de chaque côté
+        w_1 = w // 2
+        h_1 = h // 2
+        w_2 = w - w_1
+        h_2 = h - h_1
+        if x_pos - w_1 >= 0 \
+                and y_pos - h_1 >= 0 \
+                and x_pos + w_2 <= img_width \
+                and y_pos + h_2 <= img_height:
+            channels = cv2.split(fragment)
+            alpha_channel = channels[3]
+            alpha_mask = alpha_channel > 0  # on garde uniquement les pixels non à 0 sur le canal alpha
+
+            reconstructed_image[y_pos - h_1:y_pos + h_2, x_pos - w_1:x_pos + w_2][alpha_mask] = \
+                fragment[alpha_mask]
+        else:
+            print("out of range")
 
 cv2.imshow("image", reconstructed_image)
 cv2.waitKey(0)
